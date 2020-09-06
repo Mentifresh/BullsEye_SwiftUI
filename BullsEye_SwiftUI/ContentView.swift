ca777546d063d0e855ce687f508c6ab1f6ca62a1
Daniel Kilders <dankil.dev@gmail.com>
2020-09-06 17:14:09 +0200

    Added red slider

--- BullsEye_SwiftUI/ContentView.swift
+++ BullsEye_SwiftUI/ContentView.swift
@@ -9,13 +9,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let rTarget = Double.random(in: 0..<1)
+    let gTarget = Double.random(in: 0..<1)
+    let bTarget = Double.random(in: 0..<1)
+    
+    @State var rGuess: Double
+    @State var gGuess: Double
+    @State var bGuess: Double
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            HStack {
+                VStack {
+                    Color(red: rTarget, green: gTarget, blue: bTarget)
+                    Text("Match this color!")
+                }
+                VStack {
+                    Color(red: rGuess, green: gGuess, blue: bGuess)
+                    Text("R: \(Int(rGuess * 255.0))  G: \(Int(gGuess * 255.0))  B: \(Int(bGuess * 255.0))")
+                }
+            }
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("Hit Me!")
+            }
+            HStack {
+                Text("0").foregroundColor(.red)
+                Slider(value: $rGuess)
+                Text("255").foregroundColor(.red)
+            }.padding(.horizontal)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(
+            rGuess: 0.5,
+            gGuess: 0.5,
+            bGuess: 0.5
+        ).previewLayout(.fixed(width: 568, height: 320))
     }
 }
